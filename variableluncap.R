8fb138ab3c2fcdda5a946127291e988e84946909
laylaa15341 <laylaa15341@github.com>
2020-07-17 21:58:02 -0400

    adding linear reg plots

--- variableluncap.R
+++ variableluncap.R
@@ -16,4 +16,24 @@ summary(mod)
 
 Smoke <- relevel(Smoke, "yes")
 
-mod2 <- lm(LungCap ~ Age + Smoke)
+mod1 <- lm(LungCap ~ Age + Smoke)
+summary(mod1)
+
+plot(Age[Smoke=="no"], LungCap[Smoke=="no"], col="blue",
+     ylim=c(0,15), xlab="Age", ylab="LungCap", main="LungCap vs Age,Smoke",las=1)
+
+points(Age[Smoke=="yes"],LungCap[Smoke=="yes"],col="red",pch=16)
+
+legend(x=3.5,y=13.5, legend=c("Non-smoke","Smoke"),
+       col=c("blue","red"), pch=c(1,16), bty="n") 
+# add lty=c(2,3) + lwd =3 after regression lines added
+
+
+#add regression lines 
+abline(a=1.08, b=0.555, col="blue", lwd=3)
+
+abline(a=0.431, b=0.555, col="red",lwd=3)
+
+# we see no effect modification 
+
+#regression equation is -> meuw = 1.08 + (0.555xAge)-(0.649xSmoke)
